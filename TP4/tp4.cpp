28a1dc1a7ff70c128378fe42d9166bb9c48c733b
João Dossena <joaopdossena@gmail.com>
2019-11-06 12:59:21 +0000

    TP4 updates

--- TP4/tp4.cpp
+++ TP4/tp4.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
-#define ERROR 1e-2
 
 using namespace std;
 
 typedef vector<vector<double>> matrix;
 
-
-
+//Auxiliar functions
 void printMatrix(matrix &m) //Prints a matrix on the console
 {
 	unsigned int i, j;
@@ -24,7 +22,7 @@ void printMatrix(matrix &m) //Prints a matrix on the console
 }
 
 
-void rowOp(matrix &m, unsigned int a, unsigned int b, double k) //Row A = Row A - K*Row B
+void rowOp(matrix &m, unsigned int a, unsigned int b, double k) //Does the following operation: Row A = Row A - K*Row B
 {
 	unsigned int i;
 	for(i = 0; i < m[0].size(); i++)
@@ -33,7 +31,7 @@ void rowOp(matrix &m, unsigned int a, unsigned int b, double k) //Row A = Row A 
 	}
 }
 
-vector<double> gauss(matrix &m) //Nx(N+1)
+vector<double> gauss(matrix &m) //Solves a system of equations of 3x4 matrix and returns vector with solutions (x, y, z)
 {
 	vector<double> res = {0, 0, 0}; // Variables to store the results
 
@@ -57,17 +55,16 @@ vector<double> gauss(matrix &m) //Nx(N+1)
 	res.at(0) = (m[0][3] - m[0][2] * res.at(2) - m[0][1] * res.at(1))/m[0][0];
 
 	return res;
-
 }
 
-void printResult(vector<double> res)
+void printResult(vector<double> res) //Prints vector with solution (x, y, z)
 {
 	cout << "x = " << res.at(0) << endl;
 	cout << "y = " << res.at(1) << endl;
 	cout << "z = " << res.at(2) << endl << endl;
 }
 
-void printResidue(matrix &m, vector<double> res)
+void printResidue(matrix &m, vector<double> res) //Prints the difference of independent terms
 {
 	cout << "Residue: " << endl;
 	cout << "1st line: " << m[0][3] - (m[0][0] * res.at(0) + m[0][1] * res.at(1) + m[0][2] * res.at(2)) << endl;
@@ -79,24 +76,28 @@ void printResidue(matrix &m, vector<double> res)
 int main()
 {
 	vector<double> result;
-	matrix mat{{9, 1, 5, 25},
+	matrix mat{{9, 1, 5, 25}, //Declaring 3x4 matrix
 			   {1, 4, 6, 16},
 			   {2, 9, 7, 29}};
 	matrix original = mat; //Makes a copy of the matrix so we can compare it to the initial one
 
+
 	cout << "Initial matrix: " << endl; //Prints original matrix
 	printMatrix(original);
 
+
 	result = gauss(mat); //Solves matrix "mat" and puts results to the vector "result"
+
 
 	cout << "Solved matrix: " << endl; //Prints solved matrix
 	printMatrix(mat);
+
 
 	printResult(result); //Prints (x, y, z)
 
 	printResidue(original, result); //Prints residue (plugs result in matrix and compares independent terms with the original one)
 
-	
+
 
 	return 0;
 }

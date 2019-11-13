4e2e03874b9c0ba33ab369e8dd3c16382fc78e2f
João Dossena <joaopdossena@gmail.com>
2019-11-13 13:01:34 +0000

    TP5 first class

--- TP5/tp5.cpp
+++ TP5/tp5.cpp
@@ -1,10 +1,82 @@
 #include <iostream>
+//#include <cmath>
+
+#define PASSOS 100
 
 using namespace std;
 
+double f(double x) {return x*x;}
+
+double trapeziumRule(double a, double b, double n)
+{
+	double h = (b-a)/n;
+	double x = a + h;
+	double soma = f(a) + f(b), temp = 0;
+
+	for(double i = 1; i <= n - 1; i += 1)
+	{
+		temp += f(x);
+		x += h;
+		//cout << "xt: " << xt << endl;
+		//cout << "f(xt): " << f(xt) << endl;
+		//cout << "temp: " << temp << endl;
+	}
+	temp *= 2;
+	soma += temp;
+	soma = soma * h/2;
+
+	return soma;
+}
+
+double simpsonRule(double a, double b, double n)
+{
+	n = n/2;
+	double h = (b-a)/n;
+	double x = a + h;
+	double soma = f(a) + f(b), temp = 0, temp2 = 0;
+
+	for(double i = 1; i <= 2*n - 1; i += 2)
+	{
+		temp += f(x);
+		x += h;
+	}
+	temp *= 4;
+
+	for(double i = 2; i <= 2*n - 2; i += 2)
+	{
+		temp2 += f(x);
+		x += h;
+	}
+	temp2 *= 2;
+	soma += temp + temp2;
+	soma = soma * h/3;
+
+	return soma;
+}
+
 int main()
 {
-	
+	double expectedResult = 64.0/3.0;
+	double trapRule = trapeziumRule(0, 4, PASSOS);
+	double simpRule = simpsonRule(0, 4, PASSOS);
+	double Et0 = expectedResult - trapRule;
+	double Et1 = expectedResult - trapeziumRule(0, 4, PASSOS*2);
+	double Es0 = expectedResult - simpRule;
+	double Es1 = expectedResult - simpsonRule(0, 4, PASSOS*2);
+
+	cout << "Expected: " << expectedResult << endl << endl;
+
+	cout << "Trapezium Rule: " <<  trapRule << endl;
+	cout << "Et0: " << Et0 << endl;
+	cout << "Et1: " << Et1 << endl; 
+	cout << "Et0/Et1: " << Et0/Et1 << endl << endl;
+
+
+	cout << "Simpson Rule: " << simpRule << endl;
+	cout << "Es0: " << Es0 << endl;
+	cout << "Es1: " << Es1 << endl;
+	cout << "Es0/Es1: " << Es0/Es1 << endl;
+
 
 	return 0;
 }

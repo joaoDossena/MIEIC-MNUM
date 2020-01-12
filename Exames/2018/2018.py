59b331342c4f506102d08630f36e74f790ee5cb1
João Dossena <joaopdossena@gmail.com>
2020-01-12 17:59:28 +0000

    Exam of 2018 pretty much done

--- Exames/2018/2018.py
+++ Exames/2018/2018.py
@@ -76,3 +76,40 @@ def euler(x, y, z, h):
 #euler(0.4, 2, 1, 0.2)
 
 ######################## Pergunta 5 ########################
+#Plot the graph of the function on maxima to know where the function's minima is
+#Take that interval and plug it in the thirds algo, then the result of that in the sequential search, and then the intQ
+
+def f(x):
+	return (x-4)**2 + x**4
+def thirds(a, b):
+	h = (b-a)/3
+	c = a + h
+	d = b - h
+	for i in range(20):
+		if(f(a) > f(c)):
+			a = c
+		if(f(b) > f(d)):
+			b = d
+		h = (b-a)/3
+		c = a + h
+		d = b - h
+	print((a, b))
+	return
+
+def seq(a, b, n):
+	h = (b-a)/n
+	for i in range(n):
+		if(f(a+h)<f(a)):
+			a += h
+	print((a, (a+b)/2, b))
+	return 
+
+def intQ(x1, x2, x3):
+	return x2 + ((x2-x1)*(f(x1)	- (f(x3))))/(2*(f(x3) - 2*f(x2) + f(x1)))
+
+#thirds(1.1259797843176138, 1.1260173754000915)
+#seq(1.1260173640953757, 1.1260173754000915, 100000)
+#print(intQ(1.126017365249204, 1.1260173703246479, 1.1260173754000915))
+
+######################## Pergunta 6 ########################
+#I don't know
